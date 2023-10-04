fd80c5c1a52653fd39474b96ee244955f04c1591
Adriano Pessoa <adrianobpessoa@gmail.com>
2023-10-04 12:35:33 -0300

    feat: persisting data between form steps

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,34 +1,69 @@
 import {BsFillEmojiHeartEyesFill, BsFillEmojiSmileFill,BsFillEmojiFrownFill, BsFillEmojiNeutralFill} from "react-icons/bs"
 import './ReviewForm.css'
 
-const ReviewForm = () => {
+const ReviewForm = ({data, updateFieldHandler}) => {
   return (
     <div className="review-form">
         <div className="form-control score-container">
           <label className="radio-container">
-            <input type="radio" value="unstisfied" name="review" required/>
+            <input 
+              type="radio" 
+              value="unstisfied" 
+              name="review" 
+              required 
+              checked={data.review == "unstisfied"} 
+              onChange={(e) => updateFieldHandler("review", e.target.value)}
+            />
             <BsFillEmojiFrownFill/>
             <p>Insatisfeito</p>
           </label>
           <label className="radio-container">
-            <input type="radio" value="neutral" name="review" required/>
+            <input 
+              type="radio" 
+              value="neutral" 
+              name="review" 
+              required
+              checked={data.review == "neutral"} 
+              onChange={(e) => updateFieldHandler("review", e.target.value)}
+            />
             <BsFillEmojiNeutralFill/>
             <p>Poderia ser melhor</p>
           </label>
           <label className="radio-container">
-            <input type="radio" value="satisfied" name="review" required/>
+            <input 
+              type="radio" 
+              value="satisfied" 
+              name="review" 
+              required
+              checked={data.review == "satisfied"} 
+              onChange={(e) => updateFieldHandler("review", e.target.value)}
+            />
             <BsFillEmojiSmileFill/>
             <p>Satisfeito</p>
           </label>
           <label className="radio-container">
-            <input type="radio" value="very_satisfied" name="review" required/>
+            <input 
+              type="radio" 
+              value="very_satisfied" 
+              name="review" 
+              required
+              checked={data.review == "very_satisfied"} 
+              onChange={(e) => updateFieldHandler("review", e.target.value)}
+            />
             <BsFillEmojiHeartEyesFill/>
             <p>Muito satisfeito</p>
           </label>
         </div>
         <div className="form-control">
           <label htmlFor="comment">Comentário</label>
-          <textarea name="comment" id="comment" placeholder="Comte como foi a sua experiência com o produto..." required></textarea>
+          <textarea 
+            name="comment" 
+            id="comment" 
+            placeholder="Comte como foi a sua experiência com o produto..." 
+            required
+            value={data.comment || ""} 
+            onChange={(e) => updateFieldHandler("comment", e.target.value)}
+          ></textarea>
         </div>
     </div>
   )
--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,13 +1,29 @@
-const UserForm = () => {
+const UserForm = ({data, updateFieldHandler}) => {
   return (
     <div>
         <div className="form-control">
           <label htmlFor="name">Nome</label>
-          <input type="text" name="text" id="name" placeholder="Digite o seu nome" required/>
+          <input 
+            type="text" 
+            name="text" 
+            id="name" 
+            placeholder="Digite o seu nome" 
+            required 
+            value={data.name || ""} 
+            onChange={(e) => updateFieldHandler("name", e.target.value)}
+          />
         </div>
         <div className="form-control">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" placeholder="Digite o seu email" required/>
+          <input 
+            type="email" 
+            name="email" 
+            id="email" 
+            placeholder="Digite o seu email" 
+            required
+            value={data.email || ""} 
+            onChange={(e) => updateFieldHandler("email", e.target.value)}
+          />
         </div>
     </div>
   )
